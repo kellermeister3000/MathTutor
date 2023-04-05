02a28b8dc65efd077dc7da565fdcea96cc465980
Philip Keller <philipjoelkeller@gmail.com>
2023-04-04 20:01:08 -0700

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by Philip Keller on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
